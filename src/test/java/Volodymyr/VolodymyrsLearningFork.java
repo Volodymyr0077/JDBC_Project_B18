607399282f116ff1f3456f00f2a71ef8471a0c1e
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-11-01 11:36:03 -0600

    Test fork

---
+++ src/test/java/Volodymyr/VolodymyrsLearningFork.java
@@ -0,0 +1,15 @@
+package Volodymyr;
+
+public class VolodymyrsLearningFork {
+    public static void main(String[] args) {
+        int []f=new int[20];
+        f[0]=1;
+        f[1]=1;
+        for (int i=2;i<f.length;i++){
+            f[i]=f[i-1]+f[i-2];
+        }
+        for (int e:f){
+            System.out.print(e+" ");
+        }
+    }
+}
